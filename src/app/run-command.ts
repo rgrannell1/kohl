31c03a4d2ca36ffec59721ff30b2441b00b19a39
Róisín Grannell <r.grannell2@gmail.com>
2020-09-23 22:25:29 +0100

    added language

--- src/app/run-command.ts
+++ src/app/run-command.ts
@@ -4,25 +4,50 @@ import {
   KohlProps
 } from '../commons/types.js'
 
-import {float, string, whitespace} from "parjs"
-import {between, manySepBy} from "parjs/combinators"
+import P from "parsimmon"
 
-namespace ops {
-  export const undo = () => {}
-  export const redo = () => {}
-
-  export const select = () => {}
-  export const selectNot = () => {}
-
-  export const show = () => {}
-  export const showNot = () => {}
-
-  export const jumpLine = () => {}
-  export const jumpHighlight = () => {}
-
-  export const forward = () => {}
-  export const backward = () => {}
+interface Language {
+  [key: string]: (p:P.Language) => P.Parser<any>
 }
+
+const language:Language = { }
+
+language.Arg = ref => {
+  return ref.String
+}
+
+language.Call = ref => {
+  return P.seq(ref.ProcName, ref.Arg.sepBy(P.optWhitespace))
+}
+
+language.ProcName = () => {
+  return P.regexp(/[a-zA-Z0-9]+/)
+}
+
+language.Number = () => {
+  return P.regexp(/[0-9]+/).map(Number)
+}
+
+language.String = () => {
+  return P.regexp(/\"[^\"]\"/)
+}
+
+language.Jump = ref => {
+  return P.regexp(/\:[0-9]+/)
+    .sepBy(P.optWhitespace)
+    .map(data => {
+      return {
+        proc: 'jumpLine',
+        args: Number(data[0].replace(':', ''))
+      }
+    })
+}
+
+language.Value = ref => {
+  return ref.Jump
+}
+
+const lang = P.createLanguage(language)
 
 /**
  *   <expr>   :=    <jump> | <call>
@@ -36,7 +61,7 @@ namespace ops {
  * @param command
  */
 const parseCommand = (command:string) => {
-
+  return lang.Value.tryParse(command)
 }
 
 export const runCommand = (state:KohlProps, command:string) => {
