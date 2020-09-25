fc990923416a5f4339f60e50122dc71d8231dc08
Róisín Grannell <r.grannell2@gmail.com>
2020-09-25 17:13:35 +0100

    implemented isMatch

--- src/app/Line.ts
+++ src/app/Line.ts
@@ -1,6 +1,14 @@
 
 let idx = 0
 
+function isString (pattern: any): pattern is string {
+  return typeof pattern === 'string'
+}
+
+function isRegexp (pattern: any): pattern is RegExp {
+  return pattern instanceof RegExp
+}
+
 export default class Line {
   id: number
   text: string
@@ -11,11 +19,11 @@ export default class Line {
   highlight (pattern:string) {
 
   }
-  isMatch (pattern:string) {
-    if (false) {
-      // -- return regexp
-    } else {
+  isMatch (pattern:string | RegExp) {
+    if (isString(pattern)) {
       return this.text.includes(pattern)
+    } else if (isRegexp(pattern)) {
+      return pattern.test(this.text)
     }
   }
   toString () {
